43518c75ee9edea41f6cd6ce95059167cb22689c
RazAPI <apzraz452@gmail.com>
2024-08-27 20:58:28 -0700

    Update KillSwitch.lua

--- Debug/x64/Model/KillSwitch.lua
+++ Debug/x64/Model/KillSwitch.lua
@@ -1,5 +1,5 @@
 function KillSwitch()
-game.Players.LocalPlayer:Kick("Slaze is currently down for maintenance! \n Sending user to homepage...")
+game.Players.LocalPlayer:Kick("Slaze is currently down for maintenance! \n Sending user to homepage.. (3s)")
   task.wait(3)
   game:Shutdown()
 end
