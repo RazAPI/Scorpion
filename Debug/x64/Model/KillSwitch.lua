c97c9cd162071a2909973832a2ad6b2c59c98926
RazAPI <apzraz452@gmail.com>
2024-08-28 06:40:00 -0700

    Update KillSwitch.lua

--- Debug/x64/Model/KillSwitch.lua
+++ Debug/x64/Model/KillSwitch.lua
@@ -2,4 +2,3 @@ function KillSwitchActivated()
 game.Players.LocalPlayer:Kick("Slaze is currently down for maintenance, Please wait until we are back. \n Service Disabled:101:")
 end
 KillSwitchActivated = true
-Window.Visible = false
