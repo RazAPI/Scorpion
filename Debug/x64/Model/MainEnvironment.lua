285b29b2b3924b83001d1f6777fbfff6f4a66424
RazAPI <apzraz452@gmail.com>
2024-09-06 23:19:14 +0300

    Update MainEnvironment.lua

--- Debug/x64/Model/MainEnvironment.lua
+++ Debug/x64/Model/MainEnvironment.lua
@@ -17,7 +17,33 @@ getgenv().cache = {
 
 local function check(func, ...) return pcall(func, ...) end
 
--- what do you expect me to make in lua:tm:
+local dbg = {}
+function getcallingscript()
+ local s = debug.info(1, 's')
+ for i, v in next, game:GetDescendants() do
+  if v:GetFullName() == s then return v end
+ end
+ return nil
+end
+function dbg.getinfo(thread)
+ local CurrentLine = tonumber(debug.info(thread, 'l'))
+ local Source = debug.info(thread, 's')
+ local name = debug.info(thread, 'n')
+ local numparams, isvrg = debug.info(thread, 'a')
+ if #name == 0 then name = nil end
+ local a, b = debug.info(thread, 'a')
+ return {
+  ['currentline'] = CurrentLine,
+  ['Source'] = Source,
+  ['name'] = tostring(name),
+  ['numparams'] = tonumber(numparams),
+  ['is_vararg'] = isvrg and 1 or 0,
+  ['short_src'] = tostring(Source:sub(1, 60))
+  }
+end
+return dbg
+
+
 log = logserv.MessageOut:Connect(function(msg)
     if msg:find("Current identity is") then identity = tonumber(msg:gsub('Current identity is', ''):match("%d+")) end 
 end)
