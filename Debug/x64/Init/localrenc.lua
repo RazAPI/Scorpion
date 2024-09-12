8aa849a192b274fa17461d03c5566da7730491c1
RazAPI <apzraz452@gmail.com>
2024-09-12 09:13:55 +0300

    Update localrenc.lua

--- Debug/x64/Init/localrenc.lua
+++ Debug/x64/Init/localrenc.lua
@@ -1,50 +1,122 @@
 
-getgenv().getplayers = function()
+local addedfuncs = 0
+local totalfuncs = 0
+
+function check(funcName: string, func, testfunc)
+    local success, err = pcall(function()
+        getgenv()[funcName] = func
+    end)
+end
+
+check("getdevice", function()
+    return tostring(game:GetService("UserInputService"):GetPlatform()):split(".")[3]
+end, function()
+    assert(getgenv().getdevice() == tostring(game:GetService("UserInputService"):GetPlatform()):split(".")[3], "getdevice function test failed")
+end)
+
+check("getping", function(suffix: boolean)
+    local rawping = game:GetService("Stats").Network.ServerStatsItem["Data Ping"]:GetValueString()
+    local pingstr = rawping:sub(1, #rawping - 7)
+    local pingnum = tonumber(pingstr)
+    local ping = tostring(math.round(pingnum))
+    return not suffix and ping or ping .. " ms"
+end, function()
+    local ping = getgenv().getping()
+    assert(tonumber(ping) ~= nil, "getping function test failed")
+end)
+
+check("getscriptclosure", function(module)
+    local env = getrenv()
+    local constants = env.require(module)
+    return function()
+        local copy = {}
+        for k, v in pairs(constants) do
+            copy[k] = v
+        end
+        return copy
+    end
+end)
+
+check("getfps", function(): number
+    local RunService = game:GetService("RunService")
+    local FPS: number
+    local TimeFunction = RunService:IsRunning() and time or os.clock
+
+    local LastIteration: number, Start: number
+    local FrameUpdateTable = {}
+
+    local function HeartbeatUpdate()
+        LastIteration = TimeFunction()
+        for Index = #FrameUpdateTable, 1, -1 do
+            FrameUpdateTable[Index + 1] = FrameUpdateTable[Index] >= LastIteration - 1 and FrameUpdateTable[Index] or nil
+        end
+
+        FrameUpdateTable[1] = LastIteration
+        FPS = TimeFunction() - Start >= 1 and #FrameUpdateTable or #FrameUpdateTable / (TimeFunction() - Start)
+    end
+
+    Start = TimeFunction()
+    RunService.Heartbeat:Connect(HeartbeatUpdate)
+    task.wait(1.1)
+    return FPS
+end, function()
+    local fps = getgenv().getfps()
+    assert(fps ~= nil and fps >= 0, "getfps function test failed")
+end)
+
+check("getaffiliateid", function()
+    return "moREnc"
+end, function()
+    assert(getgenv().getaffiliateid() == "moREnc", "getaffiliateid function test failed")
+end)
+
+check("getplayer", function(name: string)
+    return not name and getgenv().getplayers()["LocalPlayer"] or getgenv().getplayers()[name]
+end)
+
+check("getplayers", function()
     local players = {}
     for _, x in pairs(game:GetService("Players"):GetPlayers()) do
         players[x.Name] = x
     end
     players["LocalPlayer"] = game:GetService("Players").LocalPlayer
     return players
-end
+end, function()
+    assert(getgenv().getplayers()["LocalPlayer"] == game:GetService("Players").LocalPlayer, "getplayers function test failed")
+end)
 
-getgenv().getplayer = function(name: string): Player
-    return not name and getplayers()["LocalPlayer"] or getplayers()[name]
-end
+check("getlocalplayer", function(): Player
+    return getgenv().getplayer()
+end, function()
+    assert(getgenv().getlocalplayer() == game:GetService("Players").LocalPlayer, "getlocalplayer function test failed")
+end)
 
-getgenv().runanimation = function(animationId: any, player: Player) -- @.wyv_
-    local plr: Player = player or getplayer()
-    local humanoid: Humanoid = plr.Character:FindFirstChildOfClass("Humanoid")
-    if humanoid then
-        local animation = Instance.new("Animation")
-        animation.AnimationId = "rbxassetid://" .. tostring(animationId)
-        humanoid:LoadAnimation(animation):Play()
-    end
-end
-
-getgenv().customprint = function(text: string, properties: table, imageId: rbxasset) --[[ NOT RECOMMENDED ATM
+check("customprint", function(text: string, properties: table, imageId: rbxasset)
     print(text)
-    task.wait(.025)
-    local msg = game:GetService("CoreGui").DevConsoleMaster.DevConsoleWindow.DevConsoleUI:WaitForChild("MainView").ClientLog[tostring(#game:GetService("CoreGui").DevConsoleMaster.DevConsoleWindow.DevConsoleUI.MainView.ClientLog:GetChildren())-1].msg
-    for i, x in pairs(properties) do
-        msg[i] = x
+    task.wait(0.025)
+    local clientLog = game:GetService("CoreGui").DevConsoleMaster.DevConsoleWindow.DevConsoleUI.MainView.ClientLog
+    local childrenCount = #clientLog:GetChildren()
+    local msgIndex = childrenCount > 0 and childrenCount - 1 or 0
+    local msg = clientLog:FindFirstChild(tostring(msgIndex))
+
+    if msg then
+        for i, x in pairs(properties) do
+            msg[i] = x
+        end
+        if imageId then
+            msg.Parent.image.Image = imageId
+        end
     end
-    if imageId then msg.Parent.image.Image = imageId end
-    ]]
-end
+end)
 
-getgenv().getlocalplayer = function(): Player
-    return getplayer()
-end
-
-getgenv().join = function(placeID: number, jobID: string)
+check("join", function(placeID: number, jobID: string)
     game:GetService("TeleportService"):TeleportToPlaceInstance(placeID, jobID, getplayer())
-end
+end)
 
-getgenv().firesignal = function(instance: Instance, signalName: string, args: any)
+check("firesignal", function(instance: Instance, signalName: string, args: any)
     if instance and signalName then
         local signal = instance[signalName]
-        if signal and signal:IsA("RBXScriptSignal") then
+        if signal then
             for _, connection in ipairs(getconnections(signal)) do
                 if args then
                     connection:Fire(args)
@@ -54,16 +126,61 @@ getgenv().firesignal = function(instance: Instance, signalName: string, args: an
             end
         end
     end
-end
+end, function()
+    local button = Instance.new("TextButton")
+    local new = true
+    button.MouseButton1Click:Connect(function() new = false end) 
+    firesignal(button.MouseButton1Click)
+    assert(new, "Uses old standard")
+    firesignal(button, "MouseButton1Click")
+end)
 
-getgenv().firetouchinterest = function(part: Instance, touched: boolean)
+check("firetouchinterest", function(part: Instance, touched: boolean)
     firesignal(part, touched and "Touched" or touched == false and "TouchEnded" or "Touched")
-end
+end)
 
-getgenv().fireproximityprompt = function(prompt: Instance, triggered: boolean, hold: boolean)
+check("fireproximityprompt", function(prompt: Instance, triggered: boolean, hold: boolean)
     firesignal(prompt, hold and (triggered and "PromptButtonHoldBegan" or "PromptButtonHoldEnded") or (triggered and "Triggered" or triggered == false and "TriggerEnded" or "Triggered"))
-end
+end)
 
-getgenv().firetouchtransmitter = firetouchinterest
-getgenv().playanimation = runanimation
-getgenv().joingame, getgenv().joinserver = join, join
+check("runanimation", function(animationId: any, player: Player)
+    local plr: Player = player or getgenv().getplayer()
+    local humanoid: Humanoid = plr.Character:FindFirstChildOfClass("Humanoid")
+    if humanoid then
+        local animation = Instance.new("Animation")
+        animation.AnimationId = "rbxassetid://" .. tostring(animationId)
+        humanoid:LoadAnimation(animation):Play()
+    end
+end)
+
+check("round", function()
+    getgenv().round = math.round
+end)
+
+check("joingame", function()
+    getgenv().joingame = join
+end)
+
+check("joinserver", function()
+    getgenv().joinserver = join
+end)
+
+check("firetouchtransmitter", function()
+    getgenv().firetouchtransmitter = firetouchinterest
+end)
+
+check("getplatform", function()
+    getgenv().getplatform = getdevice
+end)
+
+check("getos", function()
+    getgenv().getos = getdevice
+end)
+
+check("playanimation", function()
+    getgenv().playanimation = runanimation
+end)
+
+check("setrbxclipboard", function()
+    getgenv().setrbxclipboard = setclipboard
+end)
