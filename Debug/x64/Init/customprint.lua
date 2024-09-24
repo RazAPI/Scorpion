a1c42b1ad2fcfac29914fc38df714e3fefeef668
ScorpionDevelopment <apzraz452@gmail.com>
2024-09-24 21:40:35 +0300

    Update customprint.lua

--- Debug/x64/Init/customprint.lua
+++ Debug/x64/Init/customprint.lua
@@ -1,11 +1,12 @@
 -- Son of a bitch, insalada please remind your good old friend known as Carot to add some actual customprint functions. They completely work
 
-getgenv().customprint = function(text: string, properties: table, imageId: rbxasset)
+
+getgenv().sli_print = function(text)
     print(text)
     task.wait(.025)
     local msg = game:GetService("CoreGui").DevConsoleMaster.DevConsoleWindow.DevConsoleUI:WaitForChild("MainView").ClientLog[tostring(#game:GetService("CoreGui").DevConsoleMaster.DevConsoleWindow.DevConsoleUI.MainView.ClientLog:GetChildren())-1].msg
-    for i, x in pairs(properties) do
+    for i, x in pairs({TextColor3 = Color3.fromRGB(69, 165, 236)}) do
         msg[i] = x
     end
-    if imageId then msg.Parent.image.Image = imageId end
+    msg.Parent.image.Image = "rbxasset://textures/DevConsole/Info.png"
 end
