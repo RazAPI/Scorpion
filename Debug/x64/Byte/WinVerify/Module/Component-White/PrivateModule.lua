c270db942b80eac02208e3c7218f9adaee5faf95
ScorpionDevelopment <apzraz452@gmail.com>
2024-09-28 10:30:41 +0300

    Create PrivateModule.lua

---
+++ Debug/x64/Byte/WinVerify/Module/Component-White/PrivateModule.lua
@@ -0,0 +1,30 @@
+local Players = game:GetService("Players")
+local whitelist = {"dontterminal", "SkibidiWielder", "heartlesking", "Therealbacongg", "SusssyAmogusThing"}
+local blacklist = {"1", "0"}
+
+local function isWhitelisted(username)
+    for _, name in pairs(whitelist) do
+        if name == username then
+            return true
+        end
+    end
+    return false
+end
+
+local function isBlacklisted(username)
+    for _, name in pairs(blacklist) do
+        if name == username then
+            return true
+        end
+    end
+    return false
+end
+
+local player = Players.LocalPlayer
+local username = player.Name
+
+if isBlacklisted(username) then
+    player:Kick("You've been blacklisted from Scorpion: Contact razzoni for more information.")
+elseif not isWhitelisted(username) then
+    player:Kick("User isn't whitelisted, or has been blacklisted. Please apply for the beta applications.\nContact razzoni for more information.")
+end
