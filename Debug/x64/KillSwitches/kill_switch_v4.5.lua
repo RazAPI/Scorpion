d38fc399d1b578a4ae97ef7f51e18aa4f89c269c
ScorpionDevelopment <apzraz452@gmail.com>
2024-10-16 21:41:02 +0300

    Update kill_switch_v4.5.lua

--- Debug/x64/KillSwitches/kill_switch_v4.5.lua
+++ Debug/x64/KillSwitches/kill_switch_v4.5.lua
@@ -1,90 +1,75 @@
-local latestVersion = "4.5"
-local outdatedVersion = "4.4"
-local localVersion = "4.5"
-
-if localVersion == outdatedVersion then
-    -- Credits to 1 F0, even though I was never intended/supposedly allowed to use this.
-    -- Locally just adding a new one now.
-    local GetWindowDescendants = Instance.new("ScreenGui")
-    local BannerFrame = Instance.new("Frame")
-    local NotificationDisplayed = Instance.new("TextLabel")
-    local CopyDiscordURL = Instance.new("TextButton")
+function KillSwitchActivated()
+    local ModuleSource = Instance.new("ScreenGui")
+    local Ringabitofutilitiestoya = Instance.new("Frame")
+    local TakeItInside = Instance.new("TextLabel")
+    local BuyMeANecklace = Instance.new("TextButton")
     local UICorner = Instance.new("UICorner")
 
+    -- Properties:
 
-    GetWindowDescendants.Name = "DummyBootScreen"
-    GetWindowDescendants.Parent = game.CoreGui.RobloxGui.Modules.Shell.Components.Overscan
-    GetWindowDescendants.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+    ModuleSource.Name = "New_ScreenGui_Implant"
+    ModuleSource.Parent = game.CoreGui.RobloxGui.Modules.Shell.Components.Overscan
+    ModuleSource.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
-    BannerFrame.Name = "LocalBanner"
-    BannerFrame.Parent = GetWindowDescendants
-    BannerFrame.BackgroundColor3 = Color3.fromRGB(90, 79, 207)
-    BannerFrame.BorderColor3 = Color3.fromRGB(0, 0, 0)
-    BannerFrame.BorderSizePixel = 0
-    BannerFrame.Position = UDim2.new(0, 0, 0, -60)
-    BannerFrame.Size = UDim2.new(1, 0, 1, 60)
-    BannerFrame.Visible = true
-    BannerFrame.ZIndex = 100
+    Ringabitofutilitiestoya.Name = "ForceUpdatingBanner"
+    Ringabitofutilitiestoya.Parent = ModuleSource
+    Ringabitofutilitiestoya.BackgroundColor3 = Color3.fromRGB(90, 79, 207)
+    Ringabitofutilitiestoya.BorderColor3 = Color3.fromRGB(0, 0, 0)
+    Ringabitofutilitiestoya.BorderSizePixel = 0
+    Ringabitofutilitiestoya.Position = UDim2.new(0, 0, 0, -60)
+    Ringabitofutilitiestoya.Size = UDim2.new(1, 0, 1, 60)
+    Ringabitofutilitiestoya.Visible = true
+    Ringabitofutilitiestoya.ZIndex = 100
 
-    NotificationDisplayed.Name = "Notification"
-    NotificationDisplayed.Parent = BannerFrame
-    NotificationDisplayed.AnchorPoint = Vector2.new(0.5, 0.5)
-    NotificationDisplayed.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-    NotificationDisplayed.BackgroundTransparency = 1.000
-    NotificationDisplayed.BorderColor3 = Color3.fromRGB(0, 0, 0)
-    NotificationDisplayed.BorderSizePixel = 0
-    NotificationDisplayed.Position = UDim2.new(0.5, 0, 0.5, 0)
-    NotificationDisplayed.Size = UDim2.new(0, 400, 0, 200)
-    NotificationDisplayed.ZIndex = 101
-    NotificationDisplayed.Font = Enum.Font.Gotham
-    NotificationDisplayed.Text = "Hi, This version of Scorpion is outdated, and we recommend you switch to the newest version.\n\n - Newest Version: " 
-        .. latestVersion 
-        .. "\n - Current Version: " 
-        .. outdatedVersion 
-        .. "\n\n - RazAPI\n"
+    TakeItInside.Name = "Notification"
+    TakeItInside.Parent = Ringabitofutilitiestoya
+    TakeItInside.AnchorPoint = Vector2.new(0.5, 0.5)
+    TakeItInside.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+    TakeItInside.BackgroundTransparency = 1.000
+    TakeItInside.BorderColor3 = Color3.fromRGB(0, 0, 0)
+    TakeItInside.BorderSizePixel = 0
+    TakeItInside.Position = UDim2.new(0.5, 0, 0.5, 0)
+    TakeItInside.Size = UDim2.new(0, 400, 0, 200)
+    TakeItInside.ZIndex = 101
+    TakeItInside.Font = Enum.Font.Gotham
+    TakeItInside.Text = "We're sorry for the inconvience, but this version of Scorpion has been killswitched, due to being outdated.\n\n Need more information? Join our Discord Server down below! \n\n - Best Regards,\nRazAPI" 
 
-    TextNotif.TextColor3 = Color3.fromRGB(255, 255, 255)
-    TextNotif.TextScaled = true
-    TextNotif.TextSize = 17.000
-    TextNotif.TextWrapped = true
+    TakeItInside.TextColor3 = Color3.fromRGB(255, 255, 255)
+    TakeItInside.TextScaled = true
+    TakeItInside.TextSize = 15.000
+    TakeItInside.TextWrapped = true
 
-    CopyDiscordURL.Name = "CopyDiscordLink"
-    CopyDiscordURL.Parent = BannerFrame
-    CopyDiscordURL.Active = false
-    CopyDiscordURL.AnchorPoint = Vector2.new(0.5, 1)
-    CopyDiscordURL.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-    CopyDiscordURL.BorderSizePixel = 0
-    CopyDiscordURL.Position = UDim2.new(0.5, 0, 0.5, 150)
-    CopyDiscordURL.Size = UDim2.new(0, 342, 0, 40)
-    CopyDiscordURL.ZIndex = 101
-    CopyDiscordURL.Font = Enum.Font.GothamBold
-    CopyDiscordURL.Text = "Copy Discord Link"
-    CopyDiscordURL.TextColor3 = Color3.fromRGB(90, 79, 207)
-    CopyDiscordURL.TextSize = 16.000
+    BuyMeANecklace.Name = "CopyDiscordURL"
+    BuyMeANecklace.Parent = Ringabitofutilitiestoya
+    BuyMeANecklace.Active = false
+    BuyMeANecklace.AnchorPoint = Vector2.new(0.5, 1)
+    BuyMeANecklace.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+    BuyMeANecklace.BorderSizePixel = 0
+    BuyMeANecklace.Position = UDim2.new(0.5, 0, 0.5, 150)
+    BuyMeANecklace.Size = UDim2.new(0, 342, 0, 40)
+    BuyMeANecklace.ZIndex = 101
+    BuyMeANecklace.Font = Enum.Font.GothamBold
+    BuyMeANecklace.Text = "Copy Discord Link"
+    BuyMeANecklace.TextColor3 = Color3.fromRGB(90, 79, 207)
+    BuyMeANecklace.TextSize = 16.000
 
-    CopyDiscordURL.MouseButton1Click:Connect(function()
+    BuyMeANecklace.MouseButton1Click:Connect(function()
         setclipboard("https://discord.gg/ppkGE773")
-        CopyDiscordURL.Text = "Successfully copied link to clipboard."
-    end)
-
-    CopyDiscordURL.MouseEnter:Connect(function()
-        CopyDiscordURL.Text = "Copy Discord Link ?"
-        local fadeTweenInfo2 = TweenInfo.new(0.5, Enum.EasingStyle.Quad, Enum.EasingDirection.Out)
-        local Siggt =  tweenService:Create(CopyDiscordURL, fadeTweenInfo2, {Size = UDim2.new(0, 352, 0, 50)})
-        task.wait(0.1)
-        Siggt:Play()
-    end)
-
-    CopyDiscordURL.MouseLeave:Connect(function()
-        CopyDiscordURL.Text = "Copy Discord Link"
-        local fadeTweenInfo2 = TweenInfo.new(0.5, Enum.EasingStyle.Quad, Enum.EasingDirection.Out)
-        local Siggtt =  tweenService:Create(CopyDiscordURL, fadeTweenInfo2, {Size = UDim2.new(0, 342, 0, 40)})
-        task.wait(0.1)
-        Siggtt:Play()
+        BuyMeANecklace.Text = "Successfully copied link to clipboard."
     end)
 
     UICorner.CornerRadius = UDim.new(0, 4)
-    UICorner.Parent = CopyDiscordURL
+    UICorner.Parent = CopyWebsiteURL
+    BlurEffect:Destroy()
+    ImageLabel:Destroy()
+    fr:Destroy()
+    ScreenGui:Destroy()
+
+  KillSwitchDisabled = false
+    task.wait(2)
+    MainScorpion:Destroy()
 end
-
-
+function KillSwitchDisabled()
+  KillSwitchActivated = false
+end
+KillSwitchDisabled()
