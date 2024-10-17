eb9fd606fb40566c20c2fb07337c6ab87df65a42
ScorpionDevelopment <apzraz452@gmail.com>
2024-10-17 19:08:38 +0300

    Update kill_switch_v4.6.lua

--- Debug/x64/KillSwitches/kill_switch_v4.6.lua
+++ Debug/x64/KillSwitches/kill_switch_v4.6.lua
@@ -1,7 +1,7 @@
 function KillSwitchActivated()
-local latestVersion = "4.600"
-local outdatedVersion = "4.599"
-local localVersion = "4.600"
+local latestVersion = "4.601"
+local outdatedVersion = "4.600"
+local localVersion = "4.601"
 
 if localVersion == outdatedVersion then
     local ModuleSource = Instance.new("ScreenGui")
