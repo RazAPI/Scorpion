cbb9f7f3a8fff7f52fb777a25e82c5d88cfd50c5
ScorpionDevelopment <apzraz452@gmail.com>
2024-10-22 21:27:32 +0300

    Update Scorpion_ImGui.lua

--- Debug/x64/Model/Bootstrapper/Scorpion_ImGui.lua
+++ Debug/x64/Model/Bootstrapper/Scorpion_ImGui.lua
@@ -235,7 +235,7 @@ function main:Begin(PROPS)
         Button_Element.BorderColor3 = Color3.fromRGB(74, 74, 83)
         
         -- Position the button below the textbox
-        Button_Element.Position = UDim2.new(0, 0, 0, TextBox_ElementInput.AbsolutePosition.Y + TextBox_ElementInput.AbsoluteSize.Y + 10)
+        Button_Element.Position = UDim2.new(1, 0, 1, 0)
         Button_Element.Size = UDim2.new(0, 100, 0, 30)  -- Adjust size as needed
         Button_Element.FontFace = Font.fromId(16658246179)
         Button_Element.Text = "Execute"  -- Set the button text
